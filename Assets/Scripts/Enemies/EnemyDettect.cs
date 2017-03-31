357ef53b953443f905b27655c3920dcd0a8b181e
Skorpi76 <skorpi76@gmail.com>
2017-03-31 19:58:12 -0400

    Enemy follows player
    Enemy follows player when player come too close.

---
+++ Assets/Scripts/Enemies/EnemyDettect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDettect : MonoBehaviour {
+    EnemyMove enemyMove;
+
+
+    // ===================================
+    void Start () {
+        enemyMove = transform.parent.GetComponent<EnemyMove>();
+	}
+
+    // ===================================
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        enemyMove.IsPlayerNear(true);
+    }
+
+    // ===================================
+    void OnTriggerExit2D(Collider2D player)
+    {
+        enemyMove.IsPlayerNear(false);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Transform player;
+    private Vector3 enemyToPlayer;
+    private bool isPlayerNear;
+    public float speed; 
+    private float angle;
+
+    // ===================================
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        isPlayerNear = false;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // ===================================
+    void Update()
+    {
+        if (isPlayerNear)
+        {
+            // Enemy Rotation
+            enemyToPlayer = transform.position - player.position;
+            angle = (Mathf.Atan2(enemyToPlayer.y, enemyToPlayer.x) * Mathf.Rad2Deg) + 90;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+          
+
+        }
+
+    }
+
+    // ===================================
+    void FixedUpdate()
+    {
+        // Enemy Movement 
+        if (isPlayerNear)
+        {
+            body.velocity = transform.up * speed;
+        }
+        else
+        {
+            body.velocity = Vector2.zero;
+        }
+    }
+
+    // ===================================
+    public void IsPlayerNear(bool isNear)
+    {
+        isPlayerNear = isNear;
+    }
+}
