bddfae2108c1341136401d9d825037b8dac5698e
Eskender Memetov <skorpi76@gmail.com>
2017-04-18 17:38:02 -0300

    Level Update
    Almost completed level, created a key -> end game functional

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private GameObject exit;
+    Exit exitScript;
+
+    void Start()
+    {
+        exit = GameObject.Find("Exit");
+        exitScript = exit.GetComponent<Exit>();
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "Player")
+        {
+            Exit.keyActive = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
