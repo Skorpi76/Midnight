881726f4ed6f76012987090d571ea63dfcdb1736
Eskender Memetov <skorpi76@gmail.com>
2017-04-21 12:12:06 -0300

    Optimization / FireLight fix
    + Experemented with plane

---
+++ Assets/timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(Time.time - startTime);
+        GetComponent<Text>().text =timeSpan.ToString();
+	}
+}
